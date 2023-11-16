1aeab2576819ef03b8c20f08ff24c21b24c4908e
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-16 17:14:44 +0800

    added Material Symbols

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Cart & Tell',
+      link: [
+        {rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'}
+      ],
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ]
