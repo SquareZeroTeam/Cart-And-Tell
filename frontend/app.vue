46bd797f5cfdea6966cbca78631cc3a40118870b
Dave Bohol <jhondave.bohol@gmail.com>
2023-11-16 10:11:54 +0800

    Added Component folder and header

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Header/>
+    <Header/>
   </div>
 </template>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+    
+</template>
