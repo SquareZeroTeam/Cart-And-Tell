a13825f31abf5123ee856959c604bfab454349a5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-21 09:58:29 +0800

    enabled protection and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet()); //protect against common backend vulnerability
+  app.use(csurf());  //protect against cross-site request forgery
+  app.enableCors();
+  await app.listen(8080);
 }
 bootstrap();
