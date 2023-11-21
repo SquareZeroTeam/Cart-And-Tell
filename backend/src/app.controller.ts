292705bf56589affb3efd0cceb7d3830ffd259bc
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-22 01:15:17 +0800

    Added Throttle Test

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PaymongoService } from './paymongo/paymongo.service';
-
+import { promisify } from 'util';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private readonly paymongo:PaymongoService ) {}
@@ -9,5 +9,15 @@ export class AppController {
   @Get()
   hello() {
     return 'Cart-And-Tell API v1.0.0'
+  }
+  @Get('throttle')
+  async throttle() {
+    const delay = promisify(setTimeout);
+
+    // Simulate a delay of 5 seconds
+    await delay(5000);
+
+    console.log('done');
+    return { message: 'ok' };
   }
 }
