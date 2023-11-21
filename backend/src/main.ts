1b03fc1db021bcd005da6f7fed960c6c2a2a5966
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-21 10:00:24 +0800

    removed deprecated proection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import * as csurf from 'csurf';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet()); //protect against common backend vulnerability
-  app.use(csurf());  //protect against cross-site request forgery
   app.enableCors();
   await app.listen(8080);
 }
