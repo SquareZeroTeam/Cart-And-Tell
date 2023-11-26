80e229088d4f120ced367c5011b719a20a97d512
Jhon Dave Bohol <86525008+Davelkling@users.noreply.github.com>
2023-11-26 19:28:16 +0800

    Added Sidebar

---
+++ frontend/components/MerchantSideBar.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="border-r-2 bg-white h-screen p-2">
+        <ul>
+            <li class="relative pl-2">
+                <input type="checkbox" class="peer absolute top-0 inset-x-0 w-full h-8 opacity-0 z-50 cursor-pointer">
+                <div class=" h-8 w-full flex items-center">
+                    <span class="material-symbols-outlined mr-2">local_shipping</span>
+                    <span class="font-semibold pr-32">Shipment</span>
+                </div>
+                <div class="border-b border-gray-300 "></div>    
+                <div class=" absolute top-[2px] right-2 transition-transform duration-350 rotate-0 peer-checked:-rotate-180 ">
+                    <span class="material-symbols-outlined h-6 w-6 ">stat_1</span>
+                </div>          
+                <ul class="overflow-hidden max-h-full peer-checked:max-h-0 pl-8">
+                    <li class="hover:text-[#2563EB]">My shipment</li>
+                    <li class="hover:text-[#2563EB]">Mass ship</li>
+                    <li class="hover:text-[#2563EB]">Shipping Setting</li>
+                </ul>
+            </li>
+            <li class="relative pl-2">
+                <input type="checkbox" class="peer absolute top-0 inset-x-0 w-full h-8 opacity-0 z-50 cursor-pointer">
+                <div class=" h-8 w-full flex items-center">
+                    <span class="material-symbols-outlined mr-2">local_shipping</span>
+                    <span class="font-semibold pr-32">Shipment</span>
+                </div>
+                <div class="border-b border-gray-300 "></div>    
+                <div class=" absolute top-[2px] right-2 transition-transform duration-350 rotate-0 peer-checked:-rotate-180 ">
+                    <span class="material-symbols-outlined h-6 w-6 ">stat_1</span>
+                </div>          
+                <ul class="overflow-hidden max-h-full peer-checked:max-h-0 pl-8">
+                    <li class="hover:text-[#2563EB]">My shipment</li>
+                    <li class="hover:text-[#2563EB]">Mass ship</li>
+                    <li class="hover:text-[#2563EB]">Shipping Setting</li>
+                </ul>
+            </li>
+            <li class="relative pl-2">
+                <input type="checkbox" class="peer absolute top-0 inset-x-0 w-full h-8 opacity-0 z-50 cursor-pointer">
+                <div class=" h-8 w-full flex items-center">
+                    <span class="material-symbols-outlined mr-2">local_shipping</span>
+                    <span class="font-semibold pr-32">Shipment</span>
+                </div>
+                <div class="border-b border-gray-300 "></div>    
+                <div class=" absolute top-[2px] right-2 transition-transform duration-350 rotate-0 peer-checked:-rotate-180 ">
+                    <span class="material-symbols-outlined h-6 w-6 ">stat_1</span>
+                </div>          
+                <ul class="overflow-hidden max-h-full peer-checked:max-h-0 pl-8">
+                    <li class="hover:text-[#2563EB]">My shipment</li>
+                    <li class="hover:text-[#2563EB]">Mass ship</li>
+                    <li class="hover:text-[#2563EB]">Shipping Setting</li>
+                </ul>
+            </li>
+        </ul>
+        
+        
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
--- frontend/pages/merchant/merchantcenter.vue
+++ frontend/pages/merchant/merchantcenter.vue
@@ -5,6 +5,11 @@
 <template>
     <div>
         <MerchantHeader/>
+        <div class="flex">
+            <MerchantSideBar/>
+            <MerchantHome/>
+        </div>
+        
     </div>
 </template>
 
