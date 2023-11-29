0788eb59aac12cc1142fc1f834b84856ad18b45f
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-29 10:28:57 +0800

    Added add to cart system

--- frontend/components/ProductDetails.vue
+++ frontend/components/ProductDetails.vue
@@ -1,3 +1,16 @@
+<!-- <script setup lang="ts">
+  defineProps({
+    product: {
+      type: Object,
+      required: true,
+    },
+  })
+  const {id} = useRoute().params;
+  const formData = ref({
+    quantity: 1,
+    products:[id]
+  })
+</script> -->
 <template>
   <div class="container mx-auto mt-8 px-4 sm:px-6 lg:px-8 mt-5rem  lg:pr-48 lg:pl-48">
     <div class="flex flex-col items-center  shadow-xl px-4 sm:px-6 lg:px-8 h-[60rem] mt-[4rem]">
@@ -28,12 +41,10 @@
           </div>
 
           <div class="flex items-center space-x-4">
-            <NuxtLink to="/user/cart">
-              <button class="flex justify-center items-center bg-[#d4d5e4] bg-opacity-70 border-2 border-[#282F7A] w-36 h-12 text-[#282F7A] mr-1rem">
+              <button @click="addToCart" class="flex justify-center items-center bg-[#d4d5e4] bg-opacity-70 border-2 border-[#282F7A] w-36 h-12 text-[#282F7A] mr-1rem">
               <span class="material-symbols-outlined text-[#282F7A]">add_shopping_cart</span>
               <p class="text-md font-medium ml-2">Add to Cart</p>
             </button>
-            </NuxtLink>
             <button class="flex justify-center items-center bg-[#282F7A] border-2 border-[#282F7A] w-36 h-12 text-white">
               <p class="text-md font-normal">Buy Now</p>
             </button>
@@ -46,10 +57,10 @@
 
 <script setup>
 import { ref } from 'vue';
-
+const {id} = useRoute().params;
 //I dont know if create ba object or nu hahah (e add yata sa user's cart schema?)
 const quantity = ref(1);
-
+const userObj = useUserObj().value;
 const increment = () => {
   quantity.value += 1;
 };
@@ -59,6 +70,26 @@ const decrement = () => {
     quantity.value -= 1;
   }
 };
+
+async function addToCart() {
+  if (!userObj.id) {
+    return await navigateTo("/login");
+  }
+  const formData = {
+    quantity: quantity.value,
+    products:[parseInt(id)]
+  }
+  const API = useRuntimeConfig().public.API;
+  const { data: result,error } = await useFetch(`${API}/user/${userObj.id}/cart`,{
+    method: 'POST',
+    body: JSON.stringify(formData),
+    headers:{
+      "Content-Type":"application/json"
+    }});
+  if (result) {
+    await navigateTo("/user/cart");
+  }
+}
 
 const { product } = defineProps(['product']);
 </script>
