bee6bee96d50efbc6fea8beeee1b05f1f134a939
Jhon Dave Bohol <86525008+Davelkling@users.noreply.github.com>
2023-12-02 03:06:15 +0800

    home structure

---
+++ frontend/components/Home.vue
@@ -0,0 +1,186 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="container mx-auto bg-green-500 mr-12">
+        <div class="flex flex-col gap-4">  
+            <div class="bg-[#282F7A] flex">
+                <p class="text-white ml-6 text-lg absolute mt-2">Introduction</p>
+                <div class="mx-auto">
+                    <img class="w-96 h-auto" src="/intro.jpg" alt="Introduction Image">
+                </div>
+            </div>
+            <div class="bg-[#282F7A]  ">
+                <div class="flex justify-center">
+                    <p class="text-white m-2 text-xl">24 Categories</p>
+                </div>
+                <div class="ml-16 mt-4 mr-16 mb-16">
+                    <div class="gap-4 flex justify-center">
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                       
+                    </div>
+                    
+                    <div class="gap-4 flex justify-center mt-10">
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                        <NuxtLink to="">
+                            <button>
+                                <div class="w-16 h-16 bg-white"></div>
+                                <p class="text-white text-center">test</p>
+                            </button>
+                        </NuxtLink>
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="bg-[#282F7A]">
+                <p class="text-white m-2 text-xl text-center">Partner Merchants</p>
+           
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ frontend/components/SideBar.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container mx-auto bg-white h-screen w-auto">
+        <div class="flex flex-col items-center mt-8 gap-4 mr-4 ml-12">
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Home</p>
+            </button>
+            </NuxtLink>
+            
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Registration</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>About Us</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Contact Us</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Policy</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Certifications</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Ratings</p>
+            </button>
+            </NuxtLink>
+
+            <NuxtLink to="">
+                <button class="w-44 h-[3rem] bg-[#6DB7FB] rounded-sm text-white font-bold text-xl mt-0.5">
+                <p>Your Cart</p>
+            </button>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
