ac86db68d874b2cb5c5f3f03573c15948d7cfc90
Jhon Dave Bohol <86525008+Davelkling@users.noreply.github.com>
2023-12-05 22:47:12 +0800

    added Registration Merchant w/ form data

--- frontend/components/UploadFile.vue
+++ frontend/components/UploadFile.vue
@@ -1,27 +1,111 @@
 <script setup lang="ts">
-import { useFileDialog } from '@vueuse/core'
+const filesData = ref<{ name: string; size: number; type: string; lastModified: number }[]>([]);
+const pdfFileUrl = ref<string | null>(null); 
+const dropZoneRef = ref<HTMLElement>();
 
-const { files, open, reset, onChange } = useFileDialog()
-onChange((files) => {
-    //unsay buhaton ani
-})
+const onDrop = (files: File[] | null) => {
+  filesData.value = [];
+  if (files && files.length > 0) {
+    const pdfFile = files[0];
+    
+    pdfFileUrl.value = URL.createObjectURL(pdfFile);
+
+    // file details
+    filesData.value = [{
+      name: pdfFile.name,
+      size: pdfFile.size,
+      type: pdfFile.type,
+      lastModified: pdfFile.lastModified,
+    }];
+  }
+};
+
+const openFileDialog = () => {
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+  fileInput.accept = 'application/pdf';
+  fileInput.multiple = false;
+
+  fileInput.addEventListener('change', (event) => {
+    const input = event.target as HTMLInputElement;
+    const files = input.files;
+    if (files && files.length > 0) {
+      onDrop(Array.from(files));
+    }
+  });
+
+  fileInput.click();
+};
+
+const resetFiles = () => {
+  filesData.value = [];
+  pdfFileUrl.value = null;
+};
 </script>
 
-<template >
-    <template v-if="files" class="flex justify-center items-center">
-        <p>You have selected: <b>{{ `${files.length} ${files.length === 1 ? 'file' : 'files'}` }}</b></p>
-        <li v-for="file of files" :key="file.name">
-        {{ file.name }}
-        </li>
-    </template>
-    <div class="">
-        <button type="button" @click="open()" class="p-1 bg-blue-500 text-white rounded-sm mr-4 m-1">
-        Upload
+<template>
+    <div class="flex flex-col sm:flex-row justify-center items-center">
+        <div class="flex flex-col m-4">
+        <!-- File upload section -->
+        <div class="w-full sm:w-96 h-56 relative border-dashed border-2 border-gray-300">
+        <div
+            ref="dropZoneRef"
+            class="flex flex-col w-full min-h-200px h-auto justify-center items-center mt-6 rounded cursor-pointer"
+        >
+            <div>
+            <div class="flex flex-wrap justify-center items-center m-4">
+                <div
+                v-for="(file, index) in filesData"
+                :key="index"
+                class="w-200px bg-black-200/10 ma-2 pa-6 flex flex-col justify-center text-center"
+                >
+                <p>{{ file.name }}</p>
+                </div>
+            </div>
+            <button
+                type="button"
+                @click="openFileDialog"
+                class="w-full h-full cursor-pointer mt-10 text-bold text-gray-500"
+            >
+                Drag or Click to Upload PDF
+            </button>
+            </div>
+        </div>
+        </div>
+
+        <!-- Reset Files button -->
+        <button
+        type="button"
+        @click="resetFiles"
+        class="p-2 m-4 bg-[#282F7A] text-white rounded-sm"
+        >
+        Reset Files
         </button>
-        <button type="button" :disabled="!files" @click="reset()" class="p-1 mt-4 bg-blue-500 text-white rounded-sm">
-            Reset
-        </button>
+
+        <!-- Responsive PDF display -->
+        
+    </div>
+    <div class="m-4 flex justify-center items-center">
+            <object
+            v-if="pdfFileUrl"
+            :data="pdfFileUrl"
+            type="application/pdf"
+            class="w-full max-w-[210mm] lg:h-[297mm] lg:w-[210mm]"
+            >
+            <p>
+                Your browser does not support PDFs. <a :href="pdfFileUrl" target="_blank" class="text-[#282F7A]">Download the PDF</a> instead.
+            </p>
+            </object>
+    </div>
     </div>
     
-    
 </template>
+
+
+    <!-- <div class="">
+        <object v-if="pdfFileUrl" :data="pdfFileUrl" type="application/pdf" class="h-[297mm] w-[210mm]">
+            <p>Your browser does not support PDFs. <a :href="pdfFileUrl" target="_blank">Download the PDF</a> instead.</p>
+        </object>
+    </div>
+    
+</template> -->
--- frontend/pages/mregistration.vue
+++ frontend/pages/mregistration.vue
@@ -1,52 +1,59 @@
-
 <script setup>
+    import { ref } from 'vue';
     import vSelect from "vue-select";
     import "vue-select/dist/vue-select.css";
     import VueDatePicker from "@vuepic/vue-datepicker";
     import "@vuepic/vue-datepicker/dist/main.css";
-    import { ref } from "vue";
-    
-    const selectedBrand = ref("");
+
+    // Data to store form values
+    const formData = ref({
+    selectedBrand: '',
+    brand: '',
+    selectedRelationship: '',
+    firstDate: '',
+    secondDate: '',
+    selectedCategory: '',
+    });
+
     const selectBrands = ref([
-        { label: "qwe 1", value: "qwe 1" },
-        { label: "asd 2", value: "asd 2" },
-        { label: "zxc 3", value: "zxc 3" },
+    { label: "qwe 1", value: "qwe 1" },
+    { label: "asd 2", value: "asd 2" },
+    { label: "zxc 3", value: "zxc 3" },
     ]);
-    
-    const selectedCategory = ref("");
+
     const selectCategories = ref([
-        { label: "jewelry", value: "jewelry" },
-        { label: "asd 2", value: "asd 2" },
-        { label: "zxc 3", value: "zxc 3" },
+    { label: "jewelry", value: "jewelry" },
+    { label: "asd 2", value: "asd 2" },
+    { label: "zxc 3", value: "zxc 3" },
     ]);
-    
-    const formSubmitted = ref(false);
-    
+
     const relationshipOptions = ref([
-        { label: "Brand Owner", value: "Brand Owner" },
-        { label: "Exclusive Distributor", value: "Exclusive Distributor" },
-        { label: "Non-Exclusive Distributor", value: "Non-Exclusive Distributor" },
+    { label: "Brand Owner", value: "Brand Owner" },
+    { label: "Exclusive Distributor", value: "Exclusive Distributor" },
+    { label: "Non-Exclusive Distributor", value: "Non-Exclusive Distributor" },
     ]);
-    
-    const selectedRelationship = ref("");
-    
+
+    const formSubmitted = ref(false);
+
     const submitForm = () => {
-        formSubmitted.value = true;
-        if (selectedOption) {
-        // Handle submission
-        }
+    formSubmitted.value = true;
+
+
+    console.log(formData.value);
+
+
     };
-    
+
     const handleSelect = (value) => {
-        selectedOption.value = value;
+    formData.value.selectedBrand = value;
     };
-     
+
 </script>
 
 <template>
     <div>
         <Header />
-        <div class="flex flex-col mt-2 bg-gray-200 rounded-sm mx-auto mr-40 ml-40">
+        <div class="flex flex-col mt-4 mb-4 bg-gray-200 rounded-sm mx-auto mr-24 ml-24">
             <div class="flex justify-start">
             <p class="text-xl font-semibold mt-4 ml-5">
                 Join Cart & Tell with your Brand
@@ -83,21 +90,21 @@
                 />
             </div>
             <div class="m-4">
-                <p class="font-semibold"><span class="text-red-500 font-bold">*</span>
-                Relationship with the brand
+                <p class="font-semibold">
+                    <span class="text-red-500 font-bold">*</span> Relationship with the brand
                 </p>
-                <div class="flex gap-8">
-                <div v-for="(relationship, index) in relationshipOptions" :key="index">
+                <div class="flex flex-col sm:flex-row md:flex-row gap-4 mt-2 text-black">
+                    <div v-for="(relationship, index) in relationshipOptions" :key="index">
                     <input
-                    type="radio"
-                    :id="`relationship${index + 1}`"
-                    :value="relationship.value"
-                    v-model="selectedRelationship"
+                        type="radio"
+                        :id="`relationship${index + 1}`"
+                        :value="relationship.value"
+                        v-model="formData.selectedRelationship"
                     />
                     <label :for="`relationship${index + 1}`">{{ relationship.label }}</label>
-                </div>
-                </div>
+                    </div>
             </div>
+                </div>
             <div class="m-4">
                 <p class="text-gray-80 text-sm">You have been authorized by the brand
                 owner of the products to be listed on your store, as the non-exclusive
@@ -108,31 +115,22 @@
                 <p class="font-semibold mt-4"><span class="text-red-500 font-bold">*</span>
                 Upload Proof of Authenticity
                 </p>
-                <div class="w-full h-auto border-2 bg-gray-100">
-                <!-- Assuming `UploadFile` is your custom component for file upload -->
-                <UploadFile />
+                <div class="w-full h-full border-2 bg-gray-100">
+                    <UploadFile />
                 </div>
             </div>
             <div class="m-4">
-                <p class="font-semibold mt-4"><span class="text-red-500 font-bold">*</span>
-                Brand Authorization Validity Date
+                <p class="font-semibold mt-4">
+                    <span class="text-red-500 font-bold">*</span> Brand Authorization Validity Date
                 </p>
-                <div class="w-[40rem] flex gap-4 mt-1 text-black">
-                <VueDatePicker
-                    v-model="firstDate"
-                    :flow="flow"
-                    placeholder="mm/dd/yyyy"
-                ></VueDatePicker>
-                <VueDatePicker
-                    v-model="secondDate"
-                    :flow="flow"
-                    placeholder="mm/dd/yyyy"
-                ></VueDatePicker>
+                <div class="w-full sm:w-[26rem] flex flex-col sm:flex-row md:flex-row gap-4 mt-1 text-black">
+                    <VueDatePicker v-model="firstDate" :flow="flow" placeholder="mm/dd/yyyy"></VueDatePicker>
+                    <VueDatePicker v-model="secondDate" :flow="flow" placeholder="mm/dd/yyyy"></VueDatePicker>
                 </div>
             </div>
             <div class="m-4">
                 <p class="font-semibold"><span class="text-red-500">*</span> Main Category</p>
-                <div class="flex">
+                <div class="flex m">
                 <v-select
                     v-model="selectedCategory"
                     :options="selectCategories"
@@ -143,8 +141,8 @@
                 ></v-select>
                 </div>
             </div>
-            <button @click="submitForm" class="p-2 mt-4 bg-blue-500 text-white rounded-sm">Submit</button>
-            <span v-if="!selectedOption && formSubmitted" class="text-red-500">Please select an option</span>
+            <button @click="submitForm" class="p-2 m-4 bg-[#282F7A] text-white rounded-sm">Register Now</button>
+            <span v-if="!selectedOption && formSubmitted" class="text-red-500">Please Fill up the required inputs</span>
             </div>
         </div>
         </div>
