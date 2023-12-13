7e1b877b8d4eabe710b756d03b8720e8fb70017f
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-13 16:50:50 +0800

    fixed cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ import * as logger from "morgan"
 import { ExpressPeerServer } from 'peer';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet()); //protect against common backend vulnerability
   app.use(logger('dev'));
+  app.use('/peer-server', ExpressPeerServer(app.getHttpServer())); /// PeerJS Server 
+  app.use(helmet()); //protect against common backend vulnerability
   app.enableCors();
   
-  app.use('/', ExpressPeerServer(app.getHttpServer())); /// PeerJS Server 
   await app.listen(8080);
 }
 bootstrap();
