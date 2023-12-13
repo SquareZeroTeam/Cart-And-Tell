e6d941c9914f674681bf1a38478681f6a6296191
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-13 17:10:32 +0800

    fix render cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as logger from "morgan"
 import { ExpressPeerServer } from 'peer';
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger('dev'));
-  app.use('/peer-server', ExpressPeerServer(app.getHttpServer())); /// PeerJS Server 
+  app.use('/peer-server', cors(),ExpressPeerServer(app.getHttpServer())); /// PeerJS Server 
   app.use(helmet()); //protect against common backend vulnerability
   app.enableCors()
   
