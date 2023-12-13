d039487f65e270c6e85e50ca4b013375cf6f605d
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-13 19:52:39 +0800

    revert

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,9 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as logger from "morgan"
 import { ExpressPeerServer } from 'peer';
-import * as cors from 'cors';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
   app.use(logger('dev'));
-  app.use('/peer-server', cors()); // Enable CORS only for the /peer-server route
-  app.use('/peer-server', ExpressPeerServer(app.getHttpServer()));
   app.use(helmet()); //protect against common backend vulnerability
   app.enableCors()
   
