af7f44de589ab3e2e5ad20756dcdcafed2d48d25
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-31 22:47:20 +0800

    added cms handler

--- frontend/pages/ratings.vue
+++ frontend/pages/ratings.vue
@@ -1,23 +1,43 @@
-<script setup>
+<script setup lang="ts">
+const API = useRuntimeConfig().public.API;
+const { data, pending, error } = await useFetch<{ html: string }>(
+  `${API}/cms/Ratings`,
+  {
+    lazy: true,
+  }
+);
 </script>
 <template>
-    <div clas="container">
-        <Header/>
-        <div class="container mx-auto flex justify-center mt-2">
-            <div class="hidden sm:hidden lg:flex">
-            <SideBar />
-            </div>
-            <div class="flex flex-col gap-4 w-full mr-20">  
-                <div class="bg-[#282F7A]">
-                    <p class="text-white m-4">Ratings</p>
-                </div>
-            </div>
-
+  <div clas="container">
+    <Header />
+    <div class="container mx-auto flex justify-center mt-2">
+      <div class="hidden sm:hidden lg:flex">
+        <SideBar />
+      </div>
+      <div class="flex flex-col gap-4 w-full mr-20">
+        <div class="bg-[#282F7A]">
+          <p class="text-white m-4">Policy</p>
         </div>
+        <div
+          class="w-full h-full animate-pulse bg-gray-300 p-4 rounded-lg"
+          v-if="pending"
+        >
+          <div class="bg-gray-400 h-12 rounded-full mb-4 w-[50%]"></div>
+          <div class="bg-transparent h-36 rounded-full mb-4 w-[70%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[80%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[70%] ml-auto"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[60%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[70%] mx-auto"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[70%] mx-auto"></div>
+          <div class="bg-transparent h-12 rounded-full mb-4 w-[70%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[70%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[70%]"></div>
+          <div class="bg-gray-400 h-6 rounded-full mb-4 w-[55%]"></div>
+        </div>
+        <div v-if="!error" v-html="data!.html"></div>
+      </div>
     </div>
+  </div>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
