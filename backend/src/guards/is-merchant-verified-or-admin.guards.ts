d5f745894337f1621539b10a1c0b5f71da325c33
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-01-02 03:04:27 +0800

    fix merchantverified guard issue

--- backend/src/guards/is-merchant-verified-or-admin.guards.ts
+++ backend/src/guards/is-merchant-verified-or-admin.guards.ts
@@ -5,15 +5,14 @@ import { PrismaService } from 'src/db/prisma/prisma.service';
 
 @Injectable()
 export class IsMerchantVerifiedOrAdmin {
-  constructor(private readonly jwt:JwtService,private readonly prisma:PrismaService) {}
+  constructor(private readonly jwt: JwtService, private readonly prisma: PrismaService) { }
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean | Observable<boolean>> {
     const request = context.switchToHttp().getRequest();
     const token = request.headers.authorization.split(" ")[1];
-    const userObj = this.jwt.decode(token);
-    const verifyObj = await this.prisma.merchant.findUnique({where:{id:userObj.merchant.id}});
-    if (verifyObj.isVerified) {
+    const userObj = request.user;
+    if (userObj.merchant.isVerified) {
       return true
     }
     else if (userObj.email == process.env.ADMIN_EMAIL) {
