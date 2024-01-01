118969ea2bfc58c065e23e91329788d68fcc0460
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-01-02 01:47:34 +0800

    Added TinyMCExTailwind Issues

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
       ],
     }
   },
+  tailwindcss: {
+    configPath: '~/tailwind.config.js',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-swiper',
--- frontend/pages/policy.vue
+++ frontend/pages/policy.vue
@@ -35,7 +35,7 @@ const { data, pending, error } = await useFetch<{ html: string }>(
           <div class="bg-gray-400 h-6 rounded-full mb-4 w-[60%]"></div>
           <div class="bg-gray-400 h-6 rounded-full mb-4 w-[55%]"></div>
         </div>
-        <div v-if="!error && !pending" v-html="data!.html"></div>
+        <div v-if="!error && !pending" v-html="data!.html" class="no-more-tailwind"></div>
       </div>
     </div>
   </div>
