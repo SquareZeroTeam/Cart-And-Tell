3ff95ad33815ac5cd759858ef6cb3105d39931cd
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-01-01 19:13:30 +0800

    fix prisma bug

--- backend/src/authentication/auth/jwt.strategy.ts
+++ backend/src/authentication/auth/jwt.strategy.ts
@@ -1,25 +1,24 @@
 import { PassportStrategy } from "@nestjs/passport"
-import {Strategy, ExtractJwt} from "passport-jwt"
+import { Strategy, ExtractJwt } from "passport-jwt"
 import { PrismaService } from "src/db/prisma/prisma.service";
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() 
-    {
+    constructor(private readonly prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration:false,
+            ignoreExpiration: false,
             secretOrKey: process.env.JWT_SECRET,
         });
     };
-    async validate(payload:any) {
-        const prisma:PrismaService = new PrismaService();
-        const updatedUser = await prisma.user.findUnique({
-            where:{id:payload.id},
-            include:{
-                cart:{include:{product:true},},
-                merchant:{select:{id:true,userId:true,isVerified:true}},
-                _count:{select:{cart:true}}
-            }})
+    async validate(payload: any) {
+        const updatedUser = await this.prisma.user.findUnique({
+            where: { id: payload.id },
+            include: {
+                cart: { include: { product: true }, },
+                merchant: { select: { id: true, userId: true, isVerified: true } },
+                _count: { select: { cart: true } }
+            }
+        })
         return {
             id: payload.id,
             email: payload.email,
